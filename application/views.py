a597cd8d920a54357df61e4e6633d919022d544e
York <2890353332@qq.com>
2019-01-31 19:41:49 +0800

    httpserver first commit

---
+++ application/views.py
@@ -0,0 +1,11 @@
+#写各种数据处理
+import time
+
+def show_time():
+    return time.ctime()
+    
+def say_hello():
+    return "hello python"
+
+def say_bye():
+    return "Bye everyone"
---
+++ server/settings.py
@@ -0,0 +1,11 @@
+#htttpserver 的配置文件
+
+#httpserver IP
+HOST = '0.0.0.0'
+PORT = 8000
+ADDR = (HOST,PORT)
+
+#配置服务对象地址（webframe）地址
+frame_ip = '127.0.0.1'
+frame_port = 8080
+frame_address = (frame_ip,frame_port)
